402e1dab4618492d2b7fa0fe9c2ea4b08e47192e
Yasirnmn0 <yasirnmn@gmail.com>
2025-12-15 16:18:59 +0500

    resolve api issues

--- app/api/contacts/route.js
+++ app/api/contacts/route.js
@@ -1,18 +1,15 @@
+import { NextResponse } from "next/server";
 import dbConnect from "../../lib/monogdb";
 import Contact from "../../models/Contact";
 import nodemailer from "nodemailer";
 
 export async function POST(req) {
   try {
-    const { name, email, message } = await req.json();
-
     await dbConnect();
 
-    await Contact.create({
-      name,
-      email,
-      message,
-    });
+    const { name, email, message } = await req.json();
+
+    await Contact.create({ name, email, message });
 
     const transporter = nodemailer.createTransport({
       service: "gmail",
@@ -27,16 +24,16 @@ export async function POST(req) {
       to: process.env.EMAIL_USER,
       subject: "New Contact Message",
       html: `
-        <h3>New message from AJ Portfolio</h3>
+        <h3>New message from Portfolio</h3>
         <p><strong>Name:</strong> ${name}</p>
         <p><strong>Email:</strong> ${email}</p>
         <p>${message}</p>
       `,
     });
 
-    return Response.json({ success: true });
+    return NextResponse.json({ success: true });
   } catch (error) {
-    console.error(error);
-    return Response.json({ success: false }, { status: 500 });
+    console.error("CONTACT API ERROR:", error);
+    return NextResponse.json({ success: false }, { status: 500 });
   }
 }
